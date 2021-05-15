93c36fb44dde2722f172bc7e346eac2ab3056a3f
gzakhar <soccerzakharov@gmail.com>
2021-05-15 14:19:38 -0700

    [ADDED] ObjectiveFunctionCalculator.java

--- src/main/java/edu/stonybrook/redistricting/lemonkeredistricting/service/ObjectiveFunctionCalculator.java
+++ src/main/java/edu/stonybrook/redistricting/lemonkeredistricting/service/ObjectiveFunctionCalculator.java
@@ -1,14 +1,23 @@
 package edu.stonybrook.redistricting.lemonkeredistricting.service;
 
 import edu.stonybrook.redistricting.lemonkeredistricting.models.DistrictingScore;
+import edu.stonybrook.redistricting.lemonkeredistricting.models.DistrictingSummary;
+import edu.stonybrook.redistricting.lemonkeredistricting.repo.DistrictingSummaryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ObjectiveFunctionCalculator {
+
+    @Autowired
+    DistrictingSummaryRepository districtingSummaryRepository;
+
 
 
 //    Should calculate the Objectve function score for all districtings id's that are provided.
-    public static List<DistrictingScore> calculateObjectiveFunction(
+    public List<DistrictingScore> calculateObjectiveFunction(
             List<Integer> districtingIds,
             Double PopulationEquality,
             Double SplitCounties,
@@ -19,6 +28,11 @@ public class ObjectiveFunctionCalculator {
             Double PoliticalFairness
     ) {
 
+//        find all by districtingIds.
+        List<DistrictingSummary> districtingSummaries = districtingSummaryRepository
+                .findAllById((Iterable<Long>) districtingIds.iterator());
+
+//        work on districtingSummaries.
 
 //        Should return a list of class.DistrictingScore
         return null;
